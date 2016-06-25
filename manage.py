eb9582a7a8bd21fa51f841a2223f306974a4d864
BethMwangi <wanjikumwangib@gmail.com>
2016-06-25 20:56:11 +0300

    changes in new category template

---
+++ manage.py
@@ -0,0 +1,33 @@
+from flask_script import Manager, commands
+from todoapp import app
+
+manager= Manager(app)
+
+@manager.command
+def init_db():
+    with app.test_request_context():
+        from models import db
+        db.engine.echo = True
+        db.metadata.bind = db.engine
+        db.metadat.create_all(checkfirst=True)
+        
+if __name__= "__main__":
+    manager.run()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- todoapp.py
+++ todoapp.py
@@ -3,6 +3,9 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://blossomciku:coding101@localhost/todoapp'
+app.secret_key= 'bettyme'
+
+
 
 from views import *
 
