7c2ad306050c0f9c2335c8f88118a91776e4328f
BethMwangi <wanjikumwangib@gmail.com>
2016-07-28 16:21:22 +0300

    port added

--- todoapp.py
+++ todoapp.py
@@ -1,6 +1,7 @@
 
 
 from flask import Flask
+import os
 
 
 app = Flask(__name__)
@@ -11,8 +12,8 @@ from views import *
 
 if __name__ == '__main__':
 	app.debug = True
-	app.run()
-    
+	port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
     
 
 
